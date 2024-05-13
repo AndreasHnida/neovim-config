96a443375a1e9642536c5f904aab630005dbe58d
Andreas Hnida <andreas.hnida@gmail.com>
2024-05-13 08:00:51 +0200

    init

---
+++ lua/plugins/cody.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "sourcegraph/sg.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    -- If you have a recent version of lazy.nvim, you don't need to add this!
+    build = "nvim -l build/init.lua",
+    lazy = false,
+	opts = {
+		enable_cody = true,
+		accept_tos = true,
+		download_binaries = true,
+	},
+	config = function(opts)
+		require("sg").setup(opts)
+	end,
+  },
+}
